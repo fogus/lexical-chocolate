e4480cce0b3280253a2a5e52b4cf05fbb51a92e4
Fogus <mefogus@gmail.com>
2020-05-23 17:47:26 -0400

    multi tests

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject fogus/lexical-chocolate "0.0.2-SNAPSHOT"
-  :description "Macrology"
-  :url "http://www.fogus.me/"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :source-paths ["src/clojure"]
-  :test-paths   ["test/clojure"])
