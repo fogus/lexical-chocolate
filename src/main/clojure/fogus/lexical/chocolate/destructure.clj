663af90309ea81aba2d65e00f6644149d8551c1d
Fogus <mefogus@gmail.com>
2020-05-28 14:38:24 -0400

    simplify. start with vecs only

--- src/main/clojure/fogus/lexical/chocolate/destructure.clj
+++ src/main/clojure/fogus/lexical/chocolate/destructure.clj
@@ -1,6 +1,6 @@
 (ns fogus.lexical.chocolate.destructure)
 
-(declare pb)
+(declare process-bind-pair)
 
 (def reduce1 #'clojure.core/reduce1)
 
@@ -19,14 +19,14 @@
       (if (seq bs)
         (let [firstb (first bs)]
           (cond
-            (= firstb '&) (recur (pb ret (second bs) gseq)
+            (= firstb '&) (recur (process-bind-pair ret (second bs) gseq)
                                  n
                                  (nnext bs)
                                  true)
-            (= firstb :as) (pb ret (second bs) gvec)
+            (= firstb :as) (process-bind-pair ret (second bs) gvec)
             :else (if seen-rest?
                     (throw (new Exception "Unsupported binding form, only :as can follow & parameter"))
-                    (recur (pb (if has-rest
+                    (recur (process-bind-pair (if has-rest
                                  (conj ret
                                        gfirst `(first ~gseq)
                                        gseq `(next ~gseq))
@@ -40,62 +40,23 @@
                            seen-rest?))))
         ret))))
 
-(defn pmap [bvec b v]
-  (let [gmap (gensym "map__")
-        gmapseq (with-meta gmap {:tag 'clojure.lang.ISeq})
-        defaults (:or b)]
-    (loop [ret (-> bvec (conj gmap) (conj v)
-                   (conj gmap) (conj `(if (seq? ~gmap) (clojure.lang.PersistentHashMap/create (seq ~gmapseq)) ~gmap))
-                   ((fn [ret]
-                      (if (:as b)
-                        (conj ret (:as b) gmap)
-                        ret))))
-           bes (let [transforms
-                     (reduce1
-                      (fn [transforms mk]
-                        (if (keyword? mk)
-                          (let [mkns (namespace mk)
-                                mkn (name mk)]
-                            (cond (= mkn "keys") (assoc transforms mk #(keyword (or mkns (namespace %)) (name %)))
-                                  (= mkn "syms") (assoc transforms mk #(list `quote (symbol (or mkns (namespace %)) (name %))))
-                                  (= mkn "strs") (assoc transforms mk str)
-                                  :else transforms))
-                          transforms))
-                      {}
-                      (keys b))]
-                 (reduce1
-                  (fn [bes entry]
-                    (reduce1 #(assoc %1 %2 ((val entry) %2))
-                             (dissoc bes (key entry))
-                             ((key entry) bes)))
-                  (dissoc b :as :or)
-                  transforms))]
-      (if (seq bes)
-        (let [bb (key (first bes))
-              bk (val (first bes))
-              local (if (instance? clojure.lang.Named bb) (with-meta (symbol nil (name bb)) (meta bb)) bb)
-              bv (if (contains? defaults local)
-                   (list `get gmap bk (defaults local))
-                   (list `get gmap bk))]
-          (recur (if (ident? bb)
-                   (-> ret (conj local bv))
-                   (pb ret bb bv))
-                 (next bes)))
-        ret))))
-
-(defn pb [bvec b v]
+(defn process-bind-pair [bvec b v]
   (cond
     (symbol? b) (-> bvec (conj b) (conj v))
     (vector? b) (pvec bvec b v)
-    (map? b) (pmap bvec b v)
+    (map? b) (throw (new Exception. "Map bindings not currently supported."))
     :else (throw (new Exception (str "Unsupported binding form: " b)))))
 
-(defn process-entry [bvec b] (pb bvec (first b) (second b)))
+(defn process-entry [bvec b] (process-bind-pair bvec (first b) (second b)))
 
 (defn destructure [bindings]
-(let [bents (partition 2 bindings)]
-  (if (every? symbol? (map first bents))
-    bindings
-    (reduce1 process-entry [] bents))))
+  (let [bents (partition 2 bindings)]
+    (if (every? symbol? (map first bents))
+      bindings
+      (reduce1 process-entry [] bents))))
 
-()
+(comment
+
+  (process-bind-pair [] 'a 42)
+
+)
