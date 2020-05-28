ff4781685d4cbcc709a455afa07cb57f60195557
Fogus <mefogus@gmail.com>
2020-05-28 13:39:10 -0400

    Wrong pb

--- src/main/clojure/fogus/lexical/chocolate/destructure.clj
+++ src/main/clojure/fogus/lexical/chocolate/destructure.clj
@@ -84,52 +84,18 @@
         ret))))
 
 (defn pb [bvec b v]
-  (let [gmap (gensym "map__")
-        gmapseq (with-meta gmap {:tag 'clojure.lang.ISeq})
-        defaults (:or b)]
-    (loop [ret (-> bvec (conj gmap) (conj v)
-                   (conj gmap) (conj `(if (seq? ~gmap) (clojure.lang.PersistentHashMap/create (seq ~gmapseq)) ~gmap))
-                   ((fn [ret]
-                      (if (:as b)
-                        (conj ret (:as b) gmap)
-                        ret))))
-           bes (let [transforms
-                     (reduce1
-                      (fn [transforms mk]
-                        (if (keyword? mk)
-                          (let [mkns (namespace mk)
-                                mkn (name mk)]
-                            (cond (= mkn "keys") (assoc transforms mk #(keyword (or mkns (namespace %)) (name %)))
-                                  (= mkn "syms") (assoc transforms mk #(list `quote (symbol (or mkns (namespace %)) (name %))))
-                                  (= mkn "strs") (assoc transforms mk str)
-                                  :else transforms))
-                          transforms))
-                      {}
-                      (keys b))]
-                 (reduce1
-                  (fn [bes entry]
-                    (reduce1 #(assoc %1 %2 ((val entry) %2))
-                             (dissoc bes (key entry))
-                             ((key entry) bes)))
-                  (dissoc b :as :or)
-                  transforms))]
-      (if (seq bes)
-        (let [bb (key (first bes))
-              bk (val (first bes))
-              local (if (instance? clojure.lang.Named bb) (with-meta (symbol nil (name bb)) (meta bb)) bb)
-              bv (if (contains? defaults local)
-                   (list `get gmap bk (defaults local))
-                   (list `get gmap bk))]
-          (recur (if (ident? bb)
-                   (-> ret (conj local bv))
-                   (pb ret bb bv))
-                 (next bes)))
-        ret))))
+  (cond
+    (symbol? b) (-> bvec (conj b) (conj v))
+    (vector? b) (pvec bvec b v)
+    (map? b) (pmap bvec b v)
+    :else (throw (new Exception (str "Unsupported binding form: " b)))))
 
 (defn process-entry [bvec b] (pb bvec (first b) (second b)))
 
 (defn destructure [bindings]
-  (let [bents (partition 2 bindings)]
-    (if (every? symbol? (map first bents))
-      bindings
-      (reduce1 process-entry [] bents))))
+(let [bents (partition 2 bindings)]
+  (if (every? symbol? (map first bents))
+    bindings
+    (reduce1 process-entry [] bents))))
+
+()
